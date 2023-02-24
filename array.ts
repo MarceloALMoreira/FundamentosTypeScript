9f40ea17d3dd224352a1be63d271f2a507a4e935
marcelo.almeida <90574339+MarceloALMoreira@users.noreply.github.com>
2023-02-24 14:36:32 -0300

    feat array

---
+++ array.ts
@@ -0,0 +1,27 @@
+
+//Array Numericos
+const array: Array<number> = [1, 2, 3, 4]
+
+//Array Strings
+
+const Frutas: string[] = ['Maça', 'Banana', 'Uva', 'Goiaba']
+
+// console.log(Frutas.length)
+
+//Manipulando o Array
+
+
+console.log('Array Original', array)
+
+//Remover o ultimo elemento do Array
+array.pop()
+
+//Add um Elemento no Array
+array.push(5)
+
+console.log('Array Com novo Elemento', array)
+
+
+
+
+
